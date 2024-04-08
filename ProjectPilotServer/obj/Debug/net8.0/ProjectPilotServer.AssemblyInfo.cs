b932c804e4f9943750b690909f95937d12ff8767
eduakselhyv <akseli.hyvonen@edu.salpaus.fi>
2024-04-08 09:44:25 +0000

    made registering work

--- ProjectPilotServer/obj/Debug/net8.0/ProjectPilotServer.AssemblyInfo.cs
+++ ProjectPilotServer/obj/Debug/net8.0/ProjectPilotServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectPilotServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15445cc74a7d58a903771199a2a82771486bafa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectPilotServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectPilotServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
